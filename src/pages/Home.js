5ee6f7293fff24fe57046969e81ace0737f348fd
percygr <percyg@gmail.com>
2023-06-09 10:45:01 +0100

    moved home page links to pages file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,13 @@
-import HomePage from "../components/Home/home";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <div>
+      <h1>Home</h1>
+      <button>Post a Task</button>
+      <Link to="/browse">
+        <button>Volunteer</button>
+      </Link>
+    </div>
+  );
 }
