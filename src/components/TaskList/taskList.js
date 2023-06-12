efb347fdd41279b7d0d429f07e9bd78bba78e8e9
percygr <percyg@gmail.com>
2023-06-12 17:42:51 +0100

    test for netlify

--- src/components/TaskList/taskList.js
+++ src/components/TaskList/taskList.js
@@ -1,11 +1,14 @@
 import TaskCard from "../TaskCard/taskCard";
-//const filteredTasks = onlyAvailable ? tasks.filter(task => task.status_id === 1) : tasks;
 
 export default function TaskList({ tasks, onlyAvailable }) {
+  const filteredTasks = onlyAvailable
+    ? tasks.filter((task) => task.status_id === 1)
+    : tasks;
+
   return (
     <div>
       <p>This is the task list of all the task cards</p>
-      {tasks.map((task) => (
+      {filteredTasks.map((task) => (
         <div key={task.id}>
           <TaskCard task={task} />
         </div>
