7f5f0d7661a2e56e64260379f88aedec7d7e5396
bryony <bryonyshepherd@yahoo.co.uk>
2023-06-12 14:33:03 +0100

    nav bar added

--- src/components/TopNav/topNav.js
+++ src/components/TopNav/topNav.js
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
+import logo from '../../imgs/logo.png';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
 export default function TopNav() {
   return (
     <div>
-      <Link to="/">Home</Link>
+      <Navbar bg="info, myGradient" variant="dark">
+        <Navbar.Brand>
+          <img src="{logo}" width="40px" height="25px" />. Kindr
+        </Navbar.Brand>
+        <Nav>
+         <Nav.Link to="/"> Home</Nav.Link>
+         <Nav.Link to="/"> New Task </Nav.Link>
+         <Nav.Link to="/"> My tasks</Nav.Link>
+         <Nav.Link to="/"> My profile </Nav.Link>
+        </Nav>
+      </Navbar>
+      
       <p>TopNavBar</p>
     </div>
   );
