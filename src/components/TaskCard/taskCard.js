399eab93647171fb20fa47f3aa38e131eb4c134e
naomicrisp <naomicrisp2@gmail.com>
2023-06-14 15:11:28 +0100

    cards formatted thanks you matt

--- src/components/TaskCard/taskCard.js
+++ src/components/TaskCard/taskCard.js
@@ -1,5 +1,11 @@
 // import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+// import transport from "../../imgs/icons/tire.png";
+// import gardening from "../../imgs/icons/gardening.png";
+// import shopping from "../../imgs/icons/shopping-bags.png";
+// import housework from "../../imgs/icons/house.png";
+// import delivery from "../../imgs/icons/delivery-truck.png";
+// import other from "../../imgs/icons/question-mark.png";
 
 export default function TaskCard({
   task,
@@ -18,16 +24,22 @@ export default function TaskCard({
     };
   }
 
+  
   return (
-    <div className="task-card">
-      <div onClick={handleSelectTask(task.id)}>
-        <p>Id: {task.id}</p>
+      <div onClick={handleSelectTask(task.id)} className="task-card">
+
+        <div className = "image" >
+      <img className = "category-icon" src ={categoryIcons[1].image} alt = "task-icon" /> 
+        </div>
+
+        <div className = "card-info">
+        {/* <p>Id: {task.id}</p> */}
         <h2>Title: {task.title}</h2>
         <p>Duration: {task.duration}</p>
-        <p>Category: {task.category_id}</p>
+        {/* <p>Category: {task.category_id}</p> */}
         <p>Location: {task.location}</p>
         <p>Creator ID: {task.creator_id}</p>
+        </div> 
       </div>
-    </div>
   );
 }
