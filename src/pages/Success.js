3ba37ccdedda2285223ef7f6801124a308219f69
bryony <bryonyshepherd@yahoo.co.uk>
2023-06-14 16:28:16 +0100

    cogratulations text updated

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -4,7 +4,7 @@ import HeartIcon from "../components/HeartIcon/heartIcon.js";
 export default function SuccessPage() {
   return (
   <div>
-    <h1>Thanks for posting!</h1>
+    <h1>Congratulations!</h1>
       <HeartIcon />
     <Link to='/'>
       <button className="button">Home</button>
