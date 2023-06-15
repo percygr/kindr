3f6240520d7c2ea2419616e72cb731e6fdd2531a
lastcastleofbowser <123087687+lastcastleofbowser@users.noreply.github.com>
2023-06-15 15:31:57 +0100

    category tiles paged cssed

--- src/components/CategoryTiles/categoryTiles.js
+++ src/components/CategoryTiles/categoryTiles.js
@@ -12,37 +12,35 @@ export default function CategoryTiles({ setCategory, categoryIcons }) {
 
   return (
     <div>
-      <h1>Please select a category for this task</h1>
-      <div className="category-container">
-        <div className="category-container-top">
-          <div onClick={() => handleCategoryClick(1)} className="category-item">
-            <img src={categoryIcons[0].image} width="150" />
-            Transport
-          </div>
-          <div onClick={() => handleCategoryClick(2)} className="category-item">
-            <img src={categoryIcons[1].image} width="150" />
-            Gardening
-          </div>
-          <div onClick={() => handleCategoryClick(3)} className="category-item">
-            <img src={categoryIcons[2].image} width="150" />
-            Shopping
-          </div>
-        </div>
-        <div className="category-container-bottom">
-          <div onClick={() => handleCategoryClick(4)} className="category-item">
-            <img src={categoryIcons[3].image} width="150" />
-            Housework
-          </div>
-          <div onClick={() => handleCategoryClick(5)} className="category-item">
-            <img src={categoryIcons[4].image} width="150" />
-            Delivery
-          </div>
-          <div onClick={() => handleCategoryClick(6)} className="category-item">
-            <img src={categoryIcons[5].image} width="150" />
-            Other
-          </div>
-        </div>
+  <h1 className="category-h1">Please select a category for this task</h1>
+  <div className="category-container">
+    <div className="category-container-inner">
+      <div onClick={() => handleCategoryClick(1)} className="category-item">
+        <img src={categoryIcons[0].image} alt="Transport" />
+        <span>Transport</span>
+      </div>
+      <div onClick={() => handleCategoryClick(2)} className="category-item">
+        <img src={categoryIcons[1].image} alt="Gardening" />
+        <span>Gardening</span>
+      </div>
+      <div onClick={() => handleCategoryClick(3)} className="category-item">
+        <img src={categoryIcons[2].image} alt="Shopping" />
+        <span>Shopping</span>
+      </div>
+      <div onClick={() => handleCategoryClick(4)} className="category-item">
+        <img src={categoryIcons[3].image} alt="Housework" />
+        <span>Housework</span>
+      </div>
+      <div onClick={() => handleCategoryClick(5)} className="category-item">
+        <img src={categoryIcons[4].image} alt="Delivery" />
+        <span>Delivery</span>
+      </div>
+      <div onClick={() => handleCategoryClick(6)} className="category-item">
+        <img src={categoryIcons[5].image} alt="Other" />
+        <span>Other</span>
       </div>
     </div>
+  </div>
+</div>
   );
 }
