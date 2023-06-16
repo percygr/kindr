be10936ed4cfbb1a29181fe82b9c1f52ee868165
James Attrill <attrill20@gmail.com>
2023-06-16 15:33:12 +0100

    added text

--- src/pages/FAQpage.js
+++ src/pages/FAQpage.js
@@ -1,7 +1,35 @@
 export default function FAQPage() {
     return (
       <div>
-        <p>Test</p>
-      </div>
+    
+    <h1>What is the purpose of Kindr?</h1>
+
+    Many people feel like they would like to offer some help in their local area when they have a spare hour or two at the weekends, but don’t always know where this may be most needed. Many people could sometimes benefit from a friendly face assisting them with a task that is too much to complete on their own.
+
+    Kindr offers a quick and convenient way of connecting volunteers to tasks that need completing in their neighbourhood and helping to spread more kindness within communities.
+
+    <h1>What is the difference between ‘post a task’ and ‘volunteer’ on the home page?</h1>
+
+    The ‘post a task’ button is your starting point for asking for help with a task you need assistance with. When you press that button you will be given the chance to select a category for the task and then fill in information to best describe your request. Once you press submit, it will be posted to the site and you will be contacted by a volunteer soon!
+
+    The ‘volunteer’ button is the way for you to start spreading some kindness in your local area by browsing all of the available tasks. You can filter these by category by clicking on the icons at the top of the page, and scroll through until you see a task you think you can help with. Click on the task for more information and then press ‘accept’ to agree to offer your help for it!
+
+    <h1>What happens once a task has been accepted?</h1>
+
+    All your accepted tasks will move to the My Tasks page, where you can click on each task for more detailed information. The volunteer is able to contact the poster of the task via phone or e-mail and arrange a time to meet to complete the task. Once this has happened, you can both click on the task and press ‘Complete’ to finish and the task will move to the ‘Completed Tasks’ section where you can choose to delete it if you so wish.  Time to go and create or volunteer for another task and keep spreading that kindness!
+
+    <h1>What kind of tasks should I post?</h1>
+
+    Anything you like! Whether you need a lift down the shops, someone to help you trim the garden hedge or even just someone to pop over for a chat - Kindr is the place to ask for neighbourhood help.
+
+    We recommend trying to keep the tasks short and achievable in order to encourage a volunteer to offer their help for them. Try and be as descriptive as possible about the task, and include an accurate estimate for task ‘duration’ and a clear ‘location’ so your volunteer knows exactly how to help you out!
+
+    <h1>What about security?</h1>
+
+    Kindr has safety as a top priority 
+
+    We believe that people are kind
+
+    </div>
     );
   }
