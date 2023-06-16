8deb9274a23a423c3e29dfb56277c404cdd2df3f
James Attrill <attrill20@gmail.com>
2023-06-16 09:57:56 +0100

    edited success page text

--- src/components/HeartIcon/heartIcon.js
+++ src/components/HeartIcon/heartIcon.js
@@ -2,7 +2,6 @@ import hearticon from '../../imgs/icons/heart-icon.png'
 export default function HeartIcon() {
     return (
       <div>
-        <p>A heart icon goes here</p>
         <img id = "hearticon" src= {hearticon} alt = "heart-icon"/>
       </div>
     );
--- src/pages/Success.js
+++ src/pages/Success.js
@@ -5,8 +5,7 @@ import HeartIcon from "../components/HeartIcon/heartIcon.js";
 export default function SuccessPage() {
   return (
   <div className = "success">
-  
-      <h1>Congratulations!</h1>
+      <h1>Congratulations - thank you for spreading kindness!</h1>
       <HeartIcon />
     <Link to='/'>
       <button className="button">Home</button>
