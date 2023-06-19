35db1220c4b318d86f611c366e12f1a97b5cff0c
bryony <bryonyshepherd@yahoo.co.uk>
2023-06-19 17:36:07 +0100

    faq green

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -9,6 +9,7 @@ const FAQ = ({ faqs }) => {
   };
 
   return (
+    <div className="faq">
     <div className={styles.faqContainer}>
     <h2>Use this page to learn more about Kindr, click the boxes to expand the answer:</h2>
     <br></br>
@@ -30,6 +31,7 @@ const FAQ = ({ faqs }) => {
           )}
         </div>
       ))}
+    </div>
     </div>
   );
 };
