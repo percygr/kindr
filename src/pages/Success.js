af6cd1a86910a165379d251bd83309ac35f93e86
naomicrisp <naomicrisp2@gmail.com>
2023-06-19 14:36:50 +0100

    success page conditionally renders now

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -6,19 +6,58 @@ export default function SuccessPage({ successPath }) {
   const [windowWidth, windowHeight] = [window.innerWidth, window.innerHeight];
 
   console.log("successPath", successPath);
+  let successText = ""
+  let showHomeButton = false
+  let showMyProfileButton = false
+  let showViewTaskButton = false
+  let showMyTasksButton = false
+
+  if (successPath === "created") {
+    successText = "Thank you for posting a task - a volunteer will be in touch with you soon!"
+    showViewTaskButton = true
+    showMyTasksButton = true
+  }
+  if (successPath === "accepted") {
+    successText = "Thank you for accepting a task - please click 'View Task' to contact the user to arrange a time to complete the task"
+    showViewTaskButton = true
+    showMyTasksButton = true
+  }
+  if (successPath === "completed") {
+    successText = "Thank you for completing this task and spreading some kindness!"
+    showViewTaskButton = true
+    showMyTasksButton = true
+    showHomeButton = true
+  }
 
   return (
     <div className="success">
       <Confetti width={windowWidth.width} height={windowHeight.height} />
-      <h1>Congratulations - thank you for spreading kindness!</h1>
+      <h1> {successText}</h1>
 
       <HeartIcon />
+      {showHomeButton && (
       <Link to="/">
         <button className="button">Home</button>
       </Link>
-      <Link to="/mytasks">
+      )}
+
+      {showMyTasksButton && (
+      <Link to="/mytasks"> 
         <button className="button">My Tasks</button>
       </Link>
+      )}
+
+      {showViewTaskButton && (
+      <Link to="/view"> 
+        <button className="button">View Task</button>
+      </Link>
+      )}
+
+      {showMyProfileButton && (
+      <Link to="/profile"> 
+        <button className="button"> My Profile </button>
+      </Link>
+      )}
     </div>
   );
 }
