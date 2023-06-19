1093f335421875776aaf14cca5110512e490fbb4
Tom Burns <Tomulvlup@gmail.com>
2023-06-19 15:40:09 +0100

    updating home Co-authored-by: bryonyShep <bryonyShep@users.noreply.github.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,31 +4,33 @@ import illustrationVolunteer from "../imgs/illustrations/joy.png";
 //import Gif from "../imgs/gif/plant.gif";
 export default function HomePage() {
   return (
-    <div className="home">
-      <h1>How would you like to spread some kindness today?</h1>
-      <h2>Post a task that you need help with or volunteer to help</h2>
-      <br />
-      {/* <img src={Gif} alt="gif"className = "gif"/> */}
-        <div className="home-buttons">
-        <Link to="/categories">
-          <button className="button-blue">Post a Task</button>
-        </Link>
-        <Link to="/browse">
-          <button className="button-green">Volunteer</button>
-        </Link>
+      <div className="home">
+        <div className="home-text">
+        <h1 className="title">How would you like to spread kindness today?</h1>
+        <h2 className="post-or-help">Post a task that you need help with or volunteer to help</h2>
+        
+        {/* <img src={Gif} alt="gif"className = "gif"/> */}
+          <div className="home-buttons">
+          <Link to="/categories">
+            <button className="button-blue">Post a Task</button>
+          </Link>
+          <Link to="/browse">
+            <button className="button-green">Volunteer</button>
+          </Link>
+          </div>
+        </div>
+        <div className="illustrationsHome">
+          <img
+            className="illustrationPost"
+            src={illustrationPost}
+            alt="illustration"
+          />
+          <img
+            className="illustrationVolunteer"
+            src={illustrationVolunteer}
+            alt="illustration"
+          />
+        </div>
       </div>
-      <div className="illustrationsHome">
-        <img
-          className="illustrationPost"
-          src={illustrationPost}
-          alt="illustration"
-        />
-        <img
-          className="illustrationVolunteer"
-          src={illustrationVolunteer}
-          alt="illustration"
-        />
-      </div>
-    </div>
   );
 }
