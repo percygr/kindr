7c431e37e5d3f72081ff57c2fd1a863573ec7c46
naomicrisp <naomicrisp2@gmail.com>
2023-06-20 14:22:54 +0100

    tests grouped into Nav and home sections - more written, all passing

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import App from "./App";
 import Navbar from "./components/Navbar/Navbar";
 
+describe('Navbar component', () => {
 test("check 'Home' is in the nav bar", () => {
   render(<App />);
 
@@ -12,12 +13,90 @@ test("check 'Home' is in the nav bar", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test("check 'Post a Task' is on the home page", () => {
-  render(<App />);
+test('clicking logo takes user to home page', () => {
+render(<BrowserRouter>
+  <Navbar />
+</BrowserRouter>);
 
-  const buttons = screen.getByRole("button", { name: /Post a Task/i });
+  // Find the logo image element by its alt text
+  const logo = screen.getByAltText('logo');
+
+  // Simulate a click on the logo
+  fireEvent.click(logo);
+
+  // Assert that the correct page has been navigated to
+  expect(window.location.pathname).toBe('/');
 });
 
+test('clicking "New Task" button in the navbar navigates to the categories', () => {
+  render(
+     <BrowserRouter>
+       <Navbar />
+     </BrowserRouter>
+   );
+   const newTaskNav = screen.getByText('New Task');
+   fireEvent.click(newTaskNav);
+ 
+   // Assert that the correct page has been navigated to
+   expect(window.location.pathname).toBe('/categories');
+ });
+
+test('clicking "Browse Tasks" button in the navbar navigates to the browse page', () => {
+  render(
+     <BrowserRouter>
+       <Navbar />
+     </BrowserRouter>
+   );
+   const browseNav = screen.getByText('Browse Tasks');
+   fireEvent.click(browseNav);
+ 
+   // Assert that the correct page has been navigated to
+   expect(window.location.pathname).toBe('/browse');
+ });
+
+ test('clicking "My Tasks" button in the navbar navigates to the browse page', () => {
+  render(
+     <BrowserRouter>
+       <Navbar />
+     </BrowserRouter>
+   );
+   const myTasksNav = screen.getByText('My Tasks');
+   fireEvent.click(myTasksNav);
+ 
+   // Assert that the correct page has been navigated to
+   expect(window.location.pathname).toBe('/mytasks');
+ });
+
+ test('clicking "FAQ Page" button in the navbar navigates to the browse page', () => {
+  render(
+     <BrowserRouter>
+       <Navbar />
+     </BrowserRouter>
+   );
+   const faqNav = screen.getByText('FAQ Page');
+   fireEvent.click(faqNav);
+ 
+   // Assert that the correct page has been navigated to
+   expect(window.location.pathname).toBe('/FAQpage');
+ });
+
+ test('clicking "My Profile" button in the navbar navigates to the browse page', () => {
+  render(
+     <BrowserRouter>
+       <Navbar />
+     </BrowserRouter>
+   );
+   const myTasksNav = screen.getByText('My Profile');
+   fireEvent.click(myTasksNav);
+ 
+   // Assert that the correct page has been navigated to
+   expect(window.location.pathname).toBe('/my-profile');
+ });
+});
+
+
+
+describe('Homepage', () => {
 test('clicking "Post a Task" button navigates to the correct page', () => {
  render(
     <BrowserRouter>
@@ -48,30 +127,15 @@ test('clicking "Volunteer" button navigates to the correct page', () => {
   expect(window.location.pathname).toBe('/browse');
 });
 
-test('clicking logo takes user to home page', () => {
-render(<BrowserRouter>
-  <Navbar />
-</BrowserRouter>);
 
-  // Find the logo image element by its alt text
-  const logo = screen.getByAltText('logo');
-
-  // Simulate a click on the logo
-  fireEvent.click(logo);
-
-  // Assert that the correct page has been navigated to
-  expect(window.location.pathname).toBe('/');
+test("check 'Post a Task' is on the home page", () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+  const PostATaskButton = screen.getByText('Post a Task')
+  expect(PostATaskButton).toBeInTheDocument();
 });
 
-test('clicking "Browse Tasks" button in the navbar navigates to the browse page', () => {
-  render(
-     <BrowserRouter>
-       <Navbar />
-     </BrowserRouter>
-   );
-   const browseNav = screen.getByText('Browse Tasks');
-   fireEvent.click(browseNav);
- 
-   // Assert that the correct page has been navigated to
-   expect(window.location.pathname).toBe('/browse');
- });
+});
