2ac14cf25684ee32f0b7473f06604b257be41e56
bryony <bryonyshepherd@yahoo.co.uk>
2023-06-20 11:51:03 +0100

    arrows added

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./FAQ.module.css";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 
 const FAQ = ({ faqs }) => {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -9,30 +10,34 @@ const FAQ = ({ faqs }) => {
   };
 
   return (
-
     <div className="faq">
-    <div className={styles.faqContainer}>
-    <h2>Use this page to learn more about Kindr, click the boxes to expand the answer:</h2>
-    <br></br>
-      {faqs.map((faq, index) => (
-        <div
-          key={index}
-          className={`${styles.faqItem} ${
-            activeIndex === index ? styles.active : ""
-          }`}
-        >
+      <div className={styles.faqContainer}>
+        <h2>Use this page to learn more about Kindr, click the boxes to expand the answer:</h2>
+        <br></br>
+        {faqs.map((faq, index) => (
           <div
-            className={styles.question}
-            onClick={() => handleClick(index)}
+            key={index}
+            className={`${styles.faqItem} ${
+              activeIndex === index ? styles.active : ""
+            }`}
           >
-            {faq.question}
+            <div
+              className={styles.question}
+              onClick={() => handleClick(index)}
+            >
+              {faq.question}
+              {activeIndex === index ? (
+                <FaAngleUp className={`${styles.arrow} ${styles.arrowUp}`} />
+              ) : (
+                <FaAngleDown className={`${styles.arrow} ${styles.arrowDown}`} />
+              )}
+            </div>
+            {activeIndex === index && (
+              <div className={styles.answer}>{faq.answer}</div>
+            )}
           </div>
-          {activeIndex === index && (
-            <div className={styles.answer}>{faq.answer}</div>
-          )}
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   );
 };
