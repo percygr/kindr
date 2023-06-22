5d5eb355edff3b50557ab58bc8796428c9c2d323
Tom Burns <Tomulvlup@gmail.com>
2023-06-22 20:55:13 +0100

    profile image, menu active, aria labels

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,6 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "./navbar.css";
 import logo from "../../imgs/logos/logo.png";
-// import profilePic from "../../imgs/logos/profile.png";
 
 function Navbar({ handleLogout, userInfo }) {
   const navRef = useRef();
@@ -12,49 +11,48 @@ function Navbar({ handleLogout, userInfo }) {
     navRef.current.classList.toggle("responsive_nav");
   };
 
-  // Function to close the navbar
   const closeNavbar = () => {
     navRef.current.classList.remove("responsive_nav");
   };
 
   return (
-    <header>
-      <Link to="/" onClick={closeNavbar}>
+    <header role="banner">
+      <Link to="/" onClick={closeNavbar} aria-label="Navigate to Home Page">
         <img src={logo} className="logo" height="35px" alt="logo" />
       </Link>
-      <nav ref={navRef}>
-        <CustomLink to="/" onClick={closeNavbar}>
+      <nav ref={navRef} role="navigation">
+        <CustomLink to="/" onClick={closeNavbar} aria-label=" Home page">
           Home
         </CustomLink>
-        <CustomLink to="/categories" onClick={closeNavbar}>
+        <CustomLink to="/categories" onClick={closeNavbar} aria-label="New Task page">
           New Task
         </CustomLink>
-        <CustomLink to="/browse" onClick={closeNavbar}>
+        <CustomLink to="/browse" onClick={closeNavbar} aria-label="Browse Tasks page">
           Browse Tasks
         </CustomLink>
-        <CustomLink to="/mytasks" onClick={closeNavbar}>
+        <CustomLink to="/mytasks" onClick={closeNavbar} aria-label="My Tasks">
           My Tasks
         </CustomLink>
-        <CustomLink to="/FAQpage" onClick={closeNavbar}>
+        <CustomLink to="/FAQpage" onClick={closeNavbar} aria-label="FAQ Page">
           FAQ Page
         </CustomLink>
-        <CustomLink to="/my-profile" onClick={closeNavbar}>
+        <CustomLink to="/my-profile" onClick={closeNavbar} aria-label="My Profile page">
           My Profile
         </CustomLink>
 
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar} aria-label="Close Menu">
           <FaTimes />
         </button>
       </nav>
       <div className="profile-container">
-        <button className="button" onClick={handleLogout}>
+        <button className="button" onClick={handleLogout} aria-label="Logout">
           Log Out
         </button>
-        <Link to="/my-profile" onClick={closeNavbar}>
+        <Link to="/my-profile" onClick={closeNavbar} aria-label="Navigate to My Profile">
           <img src={userInfo.avatarUrl} className="profile-pic" alt="Profile" />
         </Link>
       </div>
-      <button className="nav-btn" onClick={showNavbar}>
+      <button className="nav-btn" onClick={showNavbar} aria-label="Open Menu">
         <FaBars />
       </button>
     </header>
@@ -71,6 +69,7 @@ function CustomLink({ to, children, onClick, ...props }) {
       className={isActive ? "active" : ""}
       onClick={onClick}
       {...props}
+      aria-current={isActive ? "page" : undefined}
     >
       {children}
     </Link>
